5c15cf2b6bbc5fdddc16d7be602e5c7008389176
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-09-11 16:20:59 -0700

    Input both numbers

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -3,15 +3,22 @@
 
 int main()
 {
-  //declare variables here
+
+  
+  int FirstNumber = 0;
+  int SecondNumber = 0;
+  
 
   std::cout<<"Please enter a whole number:\n";
-  //get first input using std::cin>>
+  //get first input using std::cin>a
+  std::cin>>FirstNumber;
 
   std::cout<<"Please enter another whole number:\n";
-  //get second input using std::cin>> again with a second variable
+  //get second input using std::cin>> again with a second variabla
+  std::cin>>SecondNumber;
 
   //determine if first or second is bigger and store that in a third variable
+
 
 
   std::cout<<"Of those two numbers, the biggest is: ";
