cf652af3d9acfd014b3a6de7316312e19acd4b0a
Carstar360 <clee124@ecc-jaguar.csuchico.edu>
2019-09-11 16:51:56 -0700

    Which Number is Biggest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -7,6 +7,8 @@ int main()
   
   int FirstNumber = 0;
   int SecondNumber = 0;
+  int biggest = 0;
+
   
 
   std::cout<<"Please enter a whole number:\n";
@@ -18,11 +20,31 @@ int main()
   std::cin>>SecondNumber;
 
   //determine if first or second is bigger and store that in a third variable
+   if(FirstNumber > SecondNumber)
+   {
+           biggest = FirstNumber;
+   
+ 
+   
+  }
+   else
+  {
+    biggest = SecondNumber;
+
+  }
+
+  
+   
+           //if
+
+  
 
 
 
   std::cout<<"Of those two numbers, the biggest is: ";
-  //print out the variable representing the biggest number
+  //print out the variable representing the biggest numbea
+  std::cout<<biggest;
+
 
   std::cout<<std::endl<<"Thank you for playing.\n";
   
